a6407aca19429b637ddbeb5eae8955ed4dd0612e
Nahimul Hussein <nahimul.hussein@kcl.ac.uk>
2022-10-05 17:45:36 +0100

    final commit

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from things import views
-
+#urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home, name = "home"),
