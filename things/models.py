66c76daa02ba12d9942b8ba0dde6ea0d06d05bb6
Nahimul Hussein <nahimul.hussein@kcl.ac.uk>
2022-10-05 16:31:55 +0100

    finished

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class User(AbstractUser):
+class Thing(AbstractUser):
     name =models.CharField(
         max_length = 30,
         unique = True,
