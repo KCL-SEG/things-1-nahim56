c415c2b211ef6bf50d54ba7766a58aea90e76d5e
Nahimul Hussein <nahimul.hussein@kcl.ac.uk>
2022-10-05 15:38:00 +0100

    Finished task

--- things/models.py
+++ things/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class User(AbstractUser):
+    name =models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False,
+        )
+    description=models.CharField(
+        max_length = 120,
+       )
+    quantity=models.IntegerField(
+    MinValueValidator = 0,
+    MaxValueValidator = 100,
+    )
