925eea9131ad3df13a6803c7c148ef36fb72f5b3
Nahimul Hussein <nahimul.hussein@kcl.ac.uk>
2022-10-10 23:21:34 +0100

    changed migration

--- things/migrations/0001_initial.py
+++ things/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             name='Thing',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('password', models.CharField(max_length=128, verbose_name='password', null = True)),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
